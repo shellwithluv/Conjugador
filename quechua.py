91365b57e3cc103b30d6db1a85c988d0ae3f6fa0
shellwithluv <114706984+shellwithluv@users.noreply.github.com>
2024-06-23 22:40:08 -0500

    Update quechua.py

--- quechua.py
+++ quechua.py
@@ -138,15 +138,13 @@ def conju_final(base, numero, persona, tiempo):
 
 ################################################################
 ################################################################
-quechua = list(verbos['quechua'])
-espanol = list(verbos['espanol'])
+# Añadir un input para que el usuario ingrese el verbo en quechua
+base = st.text_input("Ingresa un verbo en quechua")
 
-dict_que_esp = dict(zip(quechua, espanol))
-
-base = st.selectbox(
-    "Selecciona un verbo en quechua", quechua)
-
-st.write("el verbo seleccionado en español:", dict_que_esp[base])
+if base and base in verbos['quechua'].values:
+    # Mostrar el verbo en español
+    dict_que_esp = dict(zip(verbos['quechua'], verbos['espanol']))
+    st.write("El verbo ingresado en español es:", dict_que_esp[base])
 
 ## persona
 
